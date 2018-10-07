7594dae88c9d5230ecb65110b3f47cf4f2524d16
M4tB3 <bylinsk@gmail.com>
2018-10-07 14:29:57 +0200

    Add Second Exercise
    photo album exercise

---
+++ PhotoAlbumTarea/AlbumTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace PhotoAlbumTarea
+{
+    class AlbumTest
+    {
+        static void Main(string[] args)
+        {
+            //first album
+            var album1 = new PhotoAlbum();
+            Console.WriteLine(album1.GetNumberOfPages());
+
+            //second album
+            var album2 = new PhotoAlbum(24);
+            Console.WriteLine(album2.GetNumberOfPages());
+
+            //third album
+            var bigAlbum = new BigPhotoAlbum();
+            Console.WriteLine(bigAlbum.GetNumberOfPages());
+
+            Console.Read();
+        }
+    }
+
+    class PhotoAlbum
+    {
+        protected int numberOfPages;
+
+        public int GetNumberOfPages()
+        {
+            return numberOfPages;
+        }
+
+        public PhotoAlbum()
+        {
+            numberOfPages = 16;
+        }
+
+        public PhotoAlbum(int numberOfPages)
+        {
+            this.numberOfPages = numberOfPages;
+        }
+    }
+
+    class BigPhotoAlbum : PhotoAlbum
+    {
+        public BigPhotoAlbum()
+        {
+            numberOfPages = 64;
+        }
+    }
+}
