8eaea9b1a20fdb36175659c87c8b8ac952eb6dc4
MatBylin <bylinsk@gmail.com>
2018-10-10 19:18:10 +0200

    Add ArrayOfObjects Exercise

---
+++ ArrayOfObjects/ArrayTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ArrayOfObjects
+{
+    class ArrayTest
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            Table[] arrayTable = new Table[10];
+
+            for (int i = 0; i < arrayTable.Length; i++)
+            {
+                arrayTable[i] = new Table(rnd.Next(50, 201), rnd.Next(50, 201));
+                arrayTable[i].ShowData();
+            }
+
+            Console.Read();
+        }
+    }
+    class Table
+    {
+        private int _width, _height;
+
+        public Table(int width, int height)
+        {
+            _width = width;
+            _height = height;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("The width is: {0} and the height is: {1}", _width, _height);
+        }
+    }
+}
